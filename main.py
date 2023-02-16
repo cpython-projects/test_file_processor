238c2e8efd7001e2bc52a16b3de582be2db779ee
OST <cpython.projects@gmail.com>
2023-02-16 21:39:42 +0300

    add total_by_categories func

--- main.py
+++ main.py
@@ -5,3 +5,22 @@ def parse_file_line(line: str):
     money = float(money[:-1])
 
     return name, money, category
+
+
+def total_by_categories(file_path):
+    categories = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            _, money, category = parse_file_line(line)
+            if category not in categories:
+                categories[category] = money
+            else:
+                categories[category] += money
+
+    return categories
+
+
+if __name__ == '__main__':
+    res = total_by_categories('hw_10_test.txt')
+    for key, value in res.items():
+        print(f'{key}: {value:.2f}$')
